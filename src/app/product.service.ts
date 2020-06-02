8bb867c7e49481ebbfcc7e15a23bd23b3e4f5ca3
Adrian Popescu <adrian.popescu@solarwinds.com>
2020-06-02 15:52:13 +0300

    1

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,10 +6,10 @@ import 'rxjs/add/operator/map';
 export class ProductService {
   private _albumUrl = '../assets/album.json';
 
-  constructor(private _http : Http) { }
+  constructor(private _http : Http) {}
 
   getAlbum(id : number) {
-    return this._http.get(this._albumUrl).map((reponse) => response.json());
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
